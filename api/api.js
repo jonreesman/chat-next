057440179684f8d56e0b35d000d2208c11b85011
jonreesman <jreesman1@mac.com>
2022-09-29 06:23:56 -0500

    Added env for websockets

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.NEXT_PUBLIC_SITE_URL,
-    withCredentials: true,
+  baseURL: `http://` + process.env.NEXT_PUBLIC_SITE_URL,
+  withCredentials: true,
 });
--- api/createWebSocket.tsx
+++ api/createWebSocket.tsx
@@ -4,7 +4,7 @@ import { User } from '../types/user';
 
 export default function createWebSocket(room: Room, user: User, token: string): [SendMessage, MessageEvent<any>, string] {
     console.log(token)
-    const { sendMessage, lastMessage, readyState } = useWebSocket(`ws://127.0.0.1:8080/api/rooms/room/${room.ID}`, { 
+    const { sendMessage, lastMessage, readyState } = useWebSocket(`ws://` + process.env.NEXT_PUBLIC_SITE_URL + `/api/rooms/room/${room.ID}`, { 
             queryParams: {"user_id": user.ID, "username": user.Username, "room_token": token}
     });
     const connectionState = {
