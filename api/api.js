cb8a4b74d2dbea3447207d1ea21211d91037e9ef
jonreesman <jreesman1@mac.com>
2022-09-30 06:54:33 -0500

    Fixed Secure checks

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    (process.env.NEXT_PUBLIC_SITE_SECURE ? `https://` : `http://`) +
+    (process.env.NEXT_PUBLIC_SITE_SECURE === "True" ? `https://` : `http://`) +
     process.env.NEXT_PUBLIC_SITE_URL,
   withCredentials: true,
 });
