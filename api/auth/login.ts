0530b8949f2dbeb2c3c2f9978b863e95a1524d26
jonreesman <jreesman1@mac.com>
2022-10-07 17:59:57 -0500

    Cleaned deprecated Code

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -1,14 +1,10 @@
 import peakchat from '../api'
-//import setAuthToken from './setAuthToken'
-//import saveToken from './saveToken'
 export default async function login(username: string, password: string) {
             return peakchat.post(`/api/auth/login`, {
                 Identity: username,
                 Password: password,
             })
             .then(response => {
-                //setAuthToken(response.data.token)
-                //saveToken(response.data.token)
                 return response;
             })
             .catch(err => {
