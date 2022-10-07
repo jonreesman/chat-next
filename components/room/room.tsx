3af5969b5f6cb64f33aa46a6ec59069f3eb6267c
jonreesman <jreesman1@mac.com>
2022-10-07 18:00:24 -0500

    Added different connection states

--- components/room/room.tsx
+++ components/room/room.tsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect } from "react";
-import { Stack } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
 import createWebSocket from "../../api/createWebSocket";
 import { userContext } from "../../hooks/userContext";
 import MessageView from "./messageView";
 import ChatInput from "./chatInput";
 import { SendMessage } from "react-use-websocket"
 import { Room } from '../../types/room';
+import RoomSkeleton from '../roomSkeleton';
 
 
 type Props = {
@@ -21,14 +22,28 @@ type Websocket = {
 
 const Room: React.FC<Props> = ({ room, roomToken }) => {
   const {user} = useContext(userContext);
-  const [sendMessage, lastMessage, ] = createWebSocket(room, user, roomToken);
+  const [sendMessage, lastMessage, connectionState ] = createWebSocket(room, user, roomToken);
   useEffect(() => {
     console.log("re-render for WebSocket...")
   }, [room, user, roomToken])
 
+
+  if (connectionState == 'Closed' || connectionState == 'Closing') {
+    return (
+      <>
+        <Text>Connection Closed.</Text>
+      </>
+    )
+  }
+
   return (
     <Stack justify="flex-end" style={{ height: "100%", width: "100%" }}>
-      <MessageView room={room} lastMessage={lastMessage} user={user} />
+      {connectionState == 'Open' ?
+        <MessageView room={room} lastMessage={lastMessage} user={user} />
+        :
+        <RoomSkeleton />
+      }
+
       <ChatInput sendMessage={sendMessage} />
     </Stack>
   );
